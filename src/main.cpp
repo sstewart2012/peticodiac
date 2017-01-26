19447771e482ad7baf672a86dafc6c808e29bf76
Tony Yang <tianyuyang658@yahoo.com>
2017-01-26 15:26:38 -0800

    Working prototype solving smt problem from native input file

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
+#include <fstream>
 #include <cstdio>
 #include <cmath>
 #include <omp.h>
+#include <string.h>
 
 #include "AbstractSolver.h"
 #include "random.h"
@@ -53,15 +55,55 @@ void test_random(Solver* const solver, const int num_vars, const int num_constrs
   printf("\n");
 }
 
-void test_solver(const SolverType type, const int num_vars,
-                 const int num_constrs) {
-  Solver* solver = Solver::create(type, 2, 3);
-  solver->add_constraint( { 1, 1 });
-  solver->add_constraint( { 2, -1 });
-  solver->add_constraint( { -1, 2 });
-  solver->set_bounds(2, 2, NO_BOUND);
-  solver->set_bounds(3, 0, NO_BOUND);
-  solver->set_bounds(4, 1, NO_BOUND);
+std::vector<std::string> split(const std::string &s, char delim = ' ') {
+  std::vector<std::string> tokens;
+  for (int i = 0; i < s.length(); ++i) {
+    std::string current_string;
+    while (s[i] != delim && s[i] != '\n' && i < s.length()) {
+      current_string += s[i];
+      i++;
+    }
+    tokens.push_back(current_string);
+  }
+  return tokens;
+}
+
+void test_solver(const SolverType type, const int num_vars, const int num_constrs) {
+  Solver* solver;
+
+  std::string line;
+  std::ifstream peticodiac_file;
+  peticodiac_file.open("temp.smt2.peticodiac");
+  if (peticodiac_file.is_open()) {
+    while (getline(peticodiac_file, line)) {
+      if (strcmp(line.substr(0, 1).c_str(), "#") != 0 ) {
+        std::vector<std::string> expression_line = split(line);
+
+        if (expression_line[0].compare("p") == 0) {
+          // This is the header, create the solver
+          printf("Create solver with %s vars and %s bounds\n", expression_line[2].c_str(), expression_line[3].c_str());
+          solver = Solver::create(type, std::stoi(expression_line[2]), std::stoi(expression_line[3]));
+        } else if (expression_line[0].compare("c") == 0) {
+          // This is the constraint, add constraint as a vector<float>
+          printf("Add constraint %s\n", line.c_str());
+          std::vector<float> coefficient;
+          for (int i = 1; i < expression_line.size(); ++i) {
+            coefficient.push_back(std::stof(expression_line[i]));
+          }
+          solver->add_constraint(coefficient);
+        } else if (expression_line[0].compare("b") == 0) {
+          // This is the bound, set_bounds with index, lower bound, and upper bound
+          printf("Set bound %s\n", line.c_str());
+          int index = std::stoi(expression_line[1]);
+          std::vector<std::string> lower = split(expression_line[2], ':');
+          float lower_bound = lower.size() == 1 ? NO_BOUND : std::stof(lower[1]);
+          std::vector<std::string> upper = split(expression_line[3], ':');
+          float upper_bound = upper.size() == 1 ? NO_BOUND : std::stof(upper[1]);
+          solver->set_bounds(index, lower_bound, upper_bound);
+        }
+      }
+    }
+  }
 
   double time = -cpu_second();
   bool val = solver->solve();
@@ -90,7 +132,8 @@ int main(const int argc, const char** argv) {
     solver = Solver::create(SolverType::CUDA, num_vars, num_constrs);
 
   if (solver != nullptr) {
-    test_random(solver, num_vars, num_constrs);
+    //test_random(solver, num_vars, num_constrs);
+    test_solver(SolverType::CPU_EAGER, num_vars, num_constrs);
     delete solver;
   }
 
